b87a548bb4114a1d6be8f3d7e5eb5b8033b45d27
prasad nimse <53113990+PrasadProjects@users.noreply.github.com>
2023-02-12 18:21:39 +0530

    Add files via upload

---
+++ uber.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  4 19:22:04 2022
+
+@author: Prasad1511
+"""
+import streamlit as st
+import pandas as pd
+
+df = pd.read_csv (r'Claimants_Test.csv')
+print (df)
+
